21f5581ef0bee4b83d5f6b62b7dc0b24a2b82703
Benjamin Elsner <benny.nemeth@gmail.com>
2024-10-13 11:05:18 +0200

    add anim-color-gradient h1 | & blinking stars in background

---
+++ src/js/Main.js
@@ -0,0 +1,6 @@
+"use strict"
+
+import Stars from "./Stars.js";
+
+const stars = new Stars;
+stars.start();
---
+++ src/js/Stars.js
@@ -0,0 +1,35 @@
+"use strict"
+
+export default class Stars {
+    constructor() {
+
+        this.numStars = 100;
+        this.body = document.querySelector("#background");
+
+    }
+
+    start() {
+        console.log("Hello World")
+        this.createStars();
+    }
+
+    createStars() {
+
+        for (let i = 0; i < this.numStars; i++) {
+
+            const star = document.createElement('div');
+            star.classList.add('star');
+
+            const x = Math.random() * window.innerWidth;
+            const y = Math.random() * window.innerHeight;
+
+            star.style.left = `${x}px`;
+            star.style.top = `${y}px`;
+
+            const delay = Math.random() * 5;
+            star.style.animationDelay = `${delay}s`;
+
+            this.body.appendChild(star);
+        }
+    }
+}

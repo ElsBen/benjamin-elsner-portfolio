b3e830ab1e6b0210b6e6ea502a1a6ffe15571ea4
Benjamin Elsner <benny.nemeth@gmail.com>
2024-11-15 20:42:23 +0100

    try buggfix typing animation

--- src/js/TypingText.js
+++ src/js/TypingText.js
@@ -4,16 +4,22 @@ export default class TypingText {
 
     constructor() {
         this.textArray = [
-            "ein Junior Webentwickler mit einer Leidenschaft für kreative Lösungen, sauberen Code und neue Technologien.",
-            "Als Quereinsteiger habe ich neben meinem Hauptberuf verschiedene Kurse in HTML, CSS, JavaScript, TypeScript, Angular und Node.js absolviert.",
-            "Diese Erfahrung hat mir nicht nur solide Grundlagen, sondern auch die Fähigkeit gegeben, mich schnell in neue Technologien einzuarbeiten.",
-            "Durch eigene Projekte konnte ich mein Wissen weiter vertiefen und praktische Programmiererfahrung sammeln.",
-            "Mit diesen Erfahrungen fühle ich mich bestens vorbereitet, meine Fähigkeiten als Junior Webentwickler in die Praxis umzusetzen",
+            "ein Junior Webentwickler mit einer Leidenschaft für kreative Lösungen,",
+            "sauberen Code und neue Technologien.",
+            "Als Quereinsteiger habe ich neben meinem Hauptberuf verschiedene Kurse in HTML,",
+            "CSS, JavaScript, TypeScript, Angular und Node.js absolviert.",
+            "Diese Erfahrung hat mir nicht nur solide Grundlagen,",
+            "sondern auch die Fähigkeit gegeben, mich schnell in neue Technologien einzuarbeiten.",
+            "Durch eigene Projekte konnte ich mein Wissen",
+            "weiter vertiefen und praktische Programmiererfahrung sammeln.",
+            "Mit diesen Erfahrungen fühle ich mich bestens vorbereitet,",
+            "meine Fähigkeiten als Junior Webentwickler in die Praxis umzusetzen",
             "und aktiv zur Entwicklung innovativer Weblösungen beizutragen."
         ];
 
         this.typingAnimationElement = document.getElementById("typing-animation");
         this.cursorBuild = '<span class="cursor">|</span>';
+        this.paragraphs = document.querySelectorAll('#typing-animation > p');
         this.cursor = document.getElementsByClassName('cursor');
         this.textIndex = 0;
         this.charIndex = 0;
@@ -26,12 +32,13 @@ export default class TypingText {
     }
 
     createParagraphAndInsert() {
-        let newParagraph = document.createElement("p");
-        this.typingAnimationElement.insertAdjacentElement('beforeend', newParagraph);
+        // let newParagraph = document.createElement("p");
+        // this.typingAnimationElement.insertAdjacentElement('beforeend', newParagraph);
         if (this.charIndex > 0) {
             this.typeLine();
         } else if (this.textIndex === 0) {
-            this.typingAnimationElement.lastElementChild.innerHTML = this.cursorBuild;
+            // this.typingAnimationElement.lastElementChild.innerHTML = this.cursorBuild;
+            this.paragraphs[this.textIndex].innerHTML = this.cursorBuild;
             this.delayStart(this.typeLine.bind(this), this.delayBetweenLines * 2);
         } else {
             this.typeLine();
@@ -40,7 +47,8 @@ export default class TypingText {
 
     delayStart(delayedFunction, delay) {
         setTimeout(() => {
-            this.typingAnimationElement.lastElementChild.removeChild(this.cursor[0]);
+            // this.typingAnimationElement.lastElementChild.removeChild(this.cursor[0]);
+            // this.paragraphs[this.textIndex].removeChild(this.cursor[0]);
             delayedFunction();
         },
             delay
@@ -50,7 +58,8 @@ export default class TypingText {
     typeLine() {
         if (this.textIndex < this.textArray.length) {
             let currentText = this.textArray[this.textIndex];
-            this.typingAnimationElement.lastElementChild.innerHTML = currentText.slice(0, this.charIndex) + this.cursorBuild;
+            // this.typingAnimationElement.lastElementChild.innerHTML = currentText.slice(0, this.charIndex) + this.cursorBuild;
+            this.paragraphs[this.textIndex].innerHTML = currentText.slice(0, this.charIndex) + this.cursorBuild;
             this.charIndex++;
 
             if (this.charIndex > currentText.length) {
